0d58b927c22d7b340bfda54259cd4c7237f91185
almalexia <humanity.lost97@gmail.com>
2025-07-25 22:09:50 -0400

    defeating rayquaza enables finding hidden pokemon

--- src/dexnav.c
+++ src/dexnav.c
@@ -2044,7 +2044,12 @@ static void DrawSpeciesIcons(void)
         species = sDexNavUiDataPtr->hiddenSpecies[i];
         x = ROW_HIDDEN_ICON_X + 24 * i;
         y = ROW_HIDDEN_ICON_Y;
-        TryDrawIconInSlot(species, x, y);
+        if (FlagGet(FLAG_SYS_DETECTOR_MODE))
+            TryDrawIconInSlot(species, x, y);
+        else if (species == SPECIES_NONE || species > NUM_SPECIES)
+            CreateNoDataIcon(x, y);
+        else
+            CreateMonIcon(SPECIES_NONE, SpriteCB_MonIcon, x, y, 0, 0xFFFFFFFF, 0); //question mark if detector mode inactive
     }
 }
 
@@ -2064,7 +2069,10 @@ static u16 DexNavGetSpecies(void)
         species = sDexNavUiDataPtr->landSpecies[sDexNavUiDataPtr->cursorCol + COL_LAND_COUNT];
         break;
     case ROW_HIDDEN:
-        species = sDexNavUiDataPtr->hiddenSpecies[sDexNavUiDataPtr->cursorCol];
+        if (!FlagGet(FLAG_SYS_DETECTOR_MODE))
+            species = SPECIES_NONE;
+        else
+            species = sDexNavUiDataPtr->hiddenSpecies[sDexNavUiDataPtr->cursorCol];
         break;
     default:
         return SPECIES_NONE;
@@ -2516,7 +2524,7 @@ bool8 TryFindHiddenPokemon(void)
     u32 attempts = 0;
     u16 currSteps;
 
-    if (FlagGet(FLAG_SYS_DEXNAV_SEARCH) || GetFlashLevel() > 0)
+    if (!FlagGet(FLAG_SYS_DETECTOR_MODE) || FlagGet(FLAG_SYS_DEXNAV_SEARCH) || GetFlashLevel() > 0)
     {
         (*stepPtr) = 0;
         return FALSE;
